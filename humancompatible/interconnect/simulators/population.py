8825bebc45766e3fa17097dedb6621205b96465d
JoeJohnson01 <32313559+JoeJohnson01@users.noreply.github.com>
2024-07-04 15:49:36 +0100

    removed deprecated code from population node

--- humancompatible/interconnect/simulators/population.py
+++ humancompatible/interconnect/simulators/population.py
@@ -43,38 +43,3 @@ class Population(Node):
         plt.grid()
         plt.title("Probability function of Population")
         plt.plot(x_vals, y_vals)
-
-
-    # def __init__(self, name, max_workers=None):
-    #     self.type = "Population"
-    #     super().__init__(name=name)
-    #     self.agents = []
-    #     self.max_workers = max_workers
-
-    # def add_agent(self, agent):
-    #     """
-    #     Add an agent to the population
-
-    #     :param agent: Agent object
-
-    #     :return: None
-    #     """
-    #     self.agents.append(agent)
-    
-    # def add_agents(self, agents):
-    #     """
-    #     Add multiple agents to the population
-
-    #     :param agents: List of Agent objects
-
-    #     :return: None
-    #     """
-    #     self.agents.extend(agents)
-
-    # def step(self, signal):
-    #     if len(signal) > 0:
-    #         with ThreadPoolExecutor(max_workers=self.max_workers) as executor:
-    #             responses = list(executor.map(lambda agent: agent.step(signal), self.agents))
-    #         self.outputValue = responses
-    #     self.history.append(self.outputValue)
-    #     return self.outputValue
