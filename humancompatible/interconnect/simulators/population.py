corrected probability plot scaling

humancompatible/interconnect/simulators/population.py
@@ -40,19 +40,29 @@ class Population(Node):
 
         # Determine plot range if not provided
         if xMin is None or xMax is None:
-            # Try to find some interesting points in the function
             critical_points = []
             for const in self.logic.constants.values():
                 if isinstance(const, (int, float)):
                     critical_points.append(float(const))
             
-            # Add some arbitrary points if we don't have enough
-            critical_points.extend([-100, 0, 100])
+            if isinstance(expr, sp.Piecewise):
+                for piece in expr.args:
+                    if isinstance(piece[1], sp.StrictLessThan):
+                        critical_points.append(float(piece[1].args[1]))
+                    elif isinstance(piece[1], sp.StrictGreaterThan):
+                        critical_points.append(float(piece[1].args[1]))
+
+            if not critical_points:
+                critical_points = [-100, 0, 100]
 
             if xMin is None:
-                xMin = min(critical_points) - 10
+                xMin = min(critical_points)
             if xMax is None:
-                xMax = max(critical_points) + 10
+                xMax = max(critical_points)
+
+            xRange = xMax - xMin
+            xMin -= 0.1 * xRange
+            xMax += 0.1 * xRange
 
         # Generate x and y values for plotting
         x_vals = np.linspace(xMin, xMax, 50)
@@ -77,10 +87,14 @@ class Population(Node):
         valid_indices = [i for i, y in enumerate(y_vals) if y is not None]
         plt.plot([x_vals[i] for i in valid_indices], [y_vals[i] for i in valid_indices])
 
-        # Set y-axis limits
+        # Set x-axis limits exactly
+        plt.xlim(xMin, xMax)
+
+        # Set y-axis limits with padding
         y_min = min((y for y in y_vals if y is not None), default=0)
         y_max = max((y for y in y_vals if y is not None), default=1)
         y_range = y_max - y_min
-        plt.ylim(y_min - 0.1 * y_range, y_max + 0.1 * y_range)
+        y_padding = 0.1 * y_range
+        plt.ylim(y_min - y_padding, y_max + y_padding)
 
         plt.show()