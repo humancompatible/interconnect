5402edb850f6100625ebd2a9defaf2a8e5f64d5f
Nazarov, Rodion <130922166+rodionnv@users.noreply.github.com>
2024-10-09 11:31:20 +0200

    Update Agents' logics

--- humancompatible/interconnect/simulators/logic/agents/sigmoid_agent.py
+++ humancompatible/interconnect/simulators/logic/agents/sigmoid_agent.py
@@ -2,19 +2,19 @@ import torch
 
 
 class AgentLogic:
-    def __init__(self, sigmoid_const=1.0, noise=0.0):
-        self.tensors = {"x": torch.tensor([0.0], requires_grad=True)}
+    def __init__(self, s_const1=1.0, s_const2=0.0):
+        self.tensors = {"x": torch.tensor([0.0], requires_grad=True),
+                        "s_const1": torch.tensor([s_const1], requires_grad=True, dtype=torch.float),
+                        "s_const2": torch.tensor([s_const2], requires_grad=True, dtype=torch.float)}
         self.variables = ["x"]
-        self.sigmoid_const = sigmoid_const
-        self.noise = noise
 
     def _sigmoid(self, x):
-        return self.sigmoid_const / (1 + torch.exp(-x))
+        return self.tensors["s_const1"] / (1 + torch.exp(-x + self.tensors["s_const2"]))
 
     def forward(self, values, number_of_agents):
         self.tensors["x"] = values["x"]
 
-        random_numbers = self.noise * torch.rand(number_of_agents) + self.tensors["x"]
+        random_numbers = torch.zeros(number_of_agents) + self.tensors["x"]
         result = self._sigmoid(random_numbers)
 
         return result
