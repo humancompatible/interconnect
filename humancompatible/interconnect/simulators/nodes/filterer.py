b4b6e7f8ed9865c9bd5f011913abc1eac8b0a58b
Nazarov, Rodion <130922166+rodionnv@users.noreply.github.com>
2024-11-12 00:33:32 +0100

    Base code for contraction on average testing.

--- humancompatible/interconnect/simulators/nodes/filterer.py
+++ humancompatible/interconnect/simulators/nodes/filterer.py
@@ -52,6 +52,7 @@ class Filterer(Node):
         self.type = "Filterer"
         self._logic_check(logic)
         self.logic = logic
+        self.tensor_history = []
 
     def _step(self, signal):
         """
@@ -76,6 +77,7 @@ class Filterer(Node):
         self.outputValue = self.logic.forward(variable_values)
 
         self.history.append(self.outputValue.detach().numpy())
+        self.tensor_history.append(self.outputValue)
         return self.outputValue
 
     def _logic_check(self, logic):
