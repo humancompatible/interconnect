292b41325dec65e9c674d93e2c54c630ed110ee5
Nazarov, Rodion <130922166+rodionnv@users.noreply.github.com>
2024-12-24 21:44:20 +0100

    Multiple input signals to the contraction on average testing and distribution estimation. + better plotting.

--- humancompatible/interconnect/simulators/distribution.py
+++ humancompatible/interconnect/simulators/distribution.py
@@ -4,9 +4,8 @@ from scipy.stats import norm
 
 
 class Distribution:
-    def __init__(self, sim_class, reference_signal, iterations=100, samples=100):
+    def __init__(self, sim_class, iterations=100, samples=100):
         self.sim_class = sim_class
-        self.reference_signal = reference_signal
         self.iterations = iterations
         self.samples = samples
 
@@ -29,19 +28,26 @@ class Distribution:
         p = (1 / n) * np.sum(kernel(x - x_trn), axis=1)
         return p
 
-    def plot_kernel_density_estimation(self, x, y, hist, bins, h):
+    def plot_kernel_density_estimation(self, x, y, hist, bins, h, ref_sig):
         centers = (bins[:-1] + bins[1:]) / 2
         width = bins[:-1] - bins[1:]
-        plt.bar(centers, hist, width=width, edgecolor='k')
-        plt.plot(x.T, y.T, 'r', linewidth=2)
+        # plt.bar(centers, hist, width=width, edgecolor='k')
+        plt.plot(x.T, y.T, linewidth=2, label=f"reference signal = {ref_sig}")
         plt.title('h = {:.2f}'.format(h))
+        plt.legend()
 
-    def get_distribution(self, h):
-        x = self.generate_outputs()
-        x_range = np.arange(np.min(x), np.max(x), 0.1)
-        y = self.kernel_density_distribution(x_range, x, h)
-        hist, bins = np.histogram(x, 20, density=True)
-        # plots of the estimates
+    def get_distribution(self, h, reference_signals):
         plt.figure(figsize=(6, 4))
-        self.plot_kernel_density_estimation(x_range, y, hist, bins, h)
-        return y
+        x_range = np.arange(-10.0, 10.0, 0.1)
+        distributions = np.zeros((reference_signals.shape[0], x_range.shape[0]))
+        rn = 0
+        for ref_sig in reference_signals:
+            self.reference_signal = ref_sig
+            x = self.generate_outputs()
+            y = self.kernel_density_distribution(x_range, x, h)
+            hist, bins = np.histogram(x, 20, density=True)
+            # plots of the estimates
+            self.plot_kernel_density_estimation(x_range, y, hist, bins, h, ref_sig)
+            distributions[rn] = y
+            rn += 1
+        return distributions
