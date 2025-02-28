45210e10e34317b72f4551d3c3b2c6dbf9dd01dc
Nazarov, Rodion <130922166+rodionnv@users.noreply.github.com>
2025-02-28 14:53:11 +0100

    Add dedicated example notebook for contraction factor approximation.

---
+++ humancompatible/interconnect/simulators/logic/delays/exponentialSmoothingDelay.py
@@ -0,0 +1,15 @@
+import torch
+
+
+class ESDelayLogic:
+    def __init__(self, alpha=0.3):
+        self.tensors = {"x": torch.tensor([0.0], requires_grad=True)}
+        self.tensors = {"s": torch.tensor([0.0], requires_grad=True)}
+        self.variables = ["x"]
+        self.alpha = alpha
+
+    def forward(self, values):
+        self.tensors["x"] = values["x"]
+        self.tensors["s"] = self.tensors["x"] * self.alpha + self.tensors["s"] * (1.0 - self.alpha)
+        result = self.tensors["s"]
+        return result
